f70325ee616db8ae17bc2e81c51503d655fc0f47
Christopher Miller <christopher@transcriptic.com>
2017-08-25 09:47:32 -0700

    moveZeroes Solution

--- moveZeroes.js
+++ moveZeroes.js
@@ -1,17 +1,20 @@
 //Function that takes in a list of numbers and keeps the numerical order whilst
 //pushing the values of 0 to the end of the list.
 
+numArray = [0, 1, 0, 3, 12];
+
 function moveZeroes(nums) {
-	var sortedArray = [];
-    for (var i =0; i < nums.length; i++) {
-        if (nums[i] === 0) {
-            sortedArray.push(nums[i]);
+    nums = nums.map(function(obj) {
+        console.log(obj);
+        var index;
+        if (obj == 0) {
+            index = nums.indexOf(obj);
+            nums.splice(index, 1);
+            nums.push(0);
+            console.log(nums);
         }
-    }
-   	for (var j = 0; j < nums.length; j++) {
-   		if (!(nums[i] === 0)) {
-   			sortedArray.push(nums[i]);
-   		}
-   	}
-   	return sortedArray;
-};
+    })
+}
+
+
+moveZeroes(numArray);
